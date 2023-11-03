f82163bc984dc11a0ab96733d581b3fe55cd549a
Froches <michaelfavour418@gmail.com>
2023-11-03 05:15:27 -0700

    In-order fixed

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,16 +3,16 @@
 /**
  * binary_tree_inorder - Goes through a binary tree using in-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: func is a pointer to a function to call for each node. 
+ * @func: func is a pointer to a function to call for each node.
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
 
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
