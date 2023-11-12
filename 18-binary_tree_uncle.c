ecdc43b50e296e287d8921c43939b430191cf756
Froches <michaelfavour418@gmail.com>
2023-11-12 09:34:38 -0800

    Tree uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * f
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+		return (node->parent->left);
+	}
+}
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	return (binary_tree_sibling(node->parent));
+}
